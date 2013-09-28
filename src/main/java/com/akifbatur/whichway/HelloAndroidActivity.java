e38f8343cefb7dcbbf15ab2d342b0228ddf0f2be
Akif Batur <akfbtr@gmail.com>
2013-09-28 20:00:09 +0300

    Anlık enlem, boylam bilgisine göre internetten adres bilgisini alıp ekrana yazdırıyor.

--- src/main/java/com/akifbatur/whichway/HelloAndroidActivity.java
+++ src/main/java/com/akifbatur/whichway/HelloAndroidActivity.java
@@ -2,35 +2,51 @@ package com.akifbatur.whichway;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.widget.TextView;
 
-public class HelloAndroidActivity extends Activity {
+public class HelloAndroidActivity extends Activity 
+{
 
-    public static TextView textLat;
-    public static TextView textLong;
+	public static TextView textLat;
+	public static TextView textLong;
+	public static TextView myAddress;
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        textLat = (TextView)findViewById(R.id.textLat);
-        textLong = (TextView)findViewById(R.id.textLong);
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        LocationListener ll = new MyLocationListener();
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.akifbatur.whichway.R.menu.main, menu);
-	return true;
-    }
+		textLat = (TextView) findViewById(R.id.textLat);
+		textLong = (TextView) findViewById(R.id.textLong);
+		myAddress = (TextView) findViewById(R.id.myAddress);
+		
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+		//GPS acik mi degil mi?
+		boolean enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		if (!enabled) 
+		{
+			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			startActivity(intent);
+		}
+		
+		LocationListener ll = new MyLocationListener();
+		//GPS'i dinlemeye basla
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(com.akifbatur.whichway.R.menu.main, menu);
+		return true;
+	}
 }
-
--- src/main/java/com/akifbatur/whichway/MyLocationListener.java
+++ src/main/java/com/akifbatur/whichway/MyLocationListener.java
@@ -1,21 +1,56 @@
 package com.akifbatur.whichway;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 
-public class MyLocationListener implements LocationListener
+public class MyLocationListener extends Activity implements LocationListener 
 {
-
-	public void onLocationChanged(Location location) {
-		if(location != null)
+	public void onLocationChanged(Location location)
+	{
+		if (location != null) 
 		{
 			double pLong = location.getLongitude();
 			double pLat = location.getLatitude();
 			HelloAndroidActivity.textLat.setText(Double.toString(pLat));
 			HelloAndroidActivity.textLong.setText(Double.toString(pLong));
-		}
-		else
+
+			Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+			try 
+			{
+				List<Address> addresses = geocoder.getFromLocation(pLat,pLong, 1);
+
+				if (addresses != null) 
+				{
+					Address returnedAddress = addresses.get(0);
+					StringBuilder strReturnedAddress = new StringBuilder(
+							"Address:\n");
+					for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) 
+					{
+						strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+					}
+					HelloAndroidActivity.myAddress.setText(strReturnedAddress.toString());
+				} 
+				else 
+				{
+					HelloAndroidActivity.myAddress.setText("No Address returned!");
+				}
+			} catch (IOException e) 
+			{
+				e.printStackTrace();
+				HelloAndroidActivity.myAddress.setText("Unknown");
+			}
+
+		} 
+		else 
 		{
 			HelloAndroidActivity.textLat.setText("Unknown");
 			HelloAndroidActivity.textLong.setText("Unknown");
@@ -24,17 +59,17 @@ public class MyLocationListener implements LocationListener
 
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onProviderEnabled(String provider) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
