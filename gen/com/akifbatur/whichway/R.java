5c679f143bafde31246b17391e66e57ed6dff97f
Akif Batur <akif@akifbatur.com>
2013-11-03 18:33:48 +0200

    çalışan basit bir pusula ekranı

--- gen/com/akifbatur/whichway/R.java
+++ gen/com/akifbatur/whichway/R.java
@@ -22,20 +22,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int img_compass=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int textLat=0x7f080004;
-        public static final int textLong=0x7f080002;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080001;
+        public static final int action_settings=0x7f08000c;
+        public static final int imageViewCompass=0x7f08000a;
+        public static final int textLat=0x7f080002;
+        public static final int textLong=0x7f080001;
+        public static final int textView2=0x7f080000;
         public static final int textView3=0x7f080003;
         public static final int textView4=0x7f080005;
-        public static final int textView5=0x7f080008;
-        public static final int textView6=0x7f08000a;
-        public static final int textX=0x7f080006;
-        public static final int textY=0x7f080007;
-        public static final int textZ=0x7f080009;
+        public static final int textView5=0x7f080007;
+        public static final int textView6=0x7f080009;
+        public static final int textX=0x7f080004;
+        public static final int textY=0x7f080006;
+        public static final int textZ=0x7f080008;
+        public static final int tvHeading=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
