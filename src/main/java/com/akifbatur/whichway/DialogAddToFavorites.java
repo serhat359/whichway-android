acbe1514e32e8650b8781f03fac8ccc6fbe94293
Serhat Abdulbakioğlu <serhat359@gmail.com>
2017-09-19 12:05:16 +0300

    favorite eklerken yazı gözükmemesi problemi çözüldü

--- src/main/java/com/akifbatur/whichway/DialogAddToFavorites.java
+++ src/main/java/com/akifbatur/whichway/DialogAddToFavorites.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class DialogAddToFavorites extends DialogFragment{
@@ -35,6 +38,13 @@ public class DialogAddToFavorites extends DialogFragment{
 				// Will be overridden
 			}
 		});
+		
+		if(Build.VERSION.SDK_INT == Build.VERSION_CODES.GINGERBREAD
+				|| Build.VERSION.SDK_INT == Build.VERSION_CODES.GINGERBREAD_MR1){
+			((RadioButton)view.findViewById(R.id.radioCurrent)).setTextColor(Color.WHITE);
+			((RadioButton)view.findViewById(R.id.radioSearched)).setTextColor(Color.WHITE);
+		}
+		
 		return builder.create();
 	}
 
